60da38653863c88847fcdb4d03704491409913f2
Manja <marijacetkovic03@gmail.com>
2025-11-02 11:45:19 +0100

    Added difficulties

--- ai/ai_player.py
+++ ai/ai_player.py
@@ -1,23 +1,43 @@
-from famnit_gym.envs import mill
-from ai.minimax import minimax
-from ai.evaluations import INF
-from ai.utility import get_state_hash
+from evaluations import INF
+from minimax import minimax, get_state_hash
+import random
 
+class AIPlayer:
+    def __init__(self, player_id, difficulty="medium"):
+        self.player_id = player_id
+        self.difficulty = difficulty
+        self.depth_settings = {
+            "easy": 2,
+            "medium": 4,
+            "hard": 5  
+        }
+        self.rnd_settings = {
+            "easy": 0.3,
+            "medium": 0,
+            "hard": 0  
+        }
 
-def get_optimal_move(current_state, current_player):
-    best_score, best_move, opponent = -INF, None, 3 - current_player
-    legal_moves = current_state.legal_moves(player=current_player)
-    visited = {get_state_hash(current_state)}
-    if not legal_moves:   
-        return None
-    for move in legal_moves:
-        next_state = current_state.clone()
-        next_state.make_move(current_player, move)
-        score = minimax(next_state, opponent, False, 1, visited.copy())
+    def choose_move(self, state):
+        max_depth = self.depth_settings.get(self.difficulty)
 
-        if score > best_score:
-            best_score, best_move = score, move
+        legal_moves = state.legal_moves(self.player_id)
+        if not legal_moves:
+            return None
 
-    return best_move
+        if random.random() < self.rnd_settings.get(self.difficulty):
+            return random.choice(legal_moves)
 
+        best_score = -INF
+        best_move = None
+        opponent = 3 - self.player_id
+        visited = {get_state_hash(state)}
+
+        for move in legal_moves:
+            next_state = state.clone()
+            next_state.make_move(self.player_id, move)
+            score = minimax(next_state, opponent, False, 1, visited.copy(), max_depth)
+            if score > best_score:
+                best_score, best_move = score, move
+
+        return best_move
 
